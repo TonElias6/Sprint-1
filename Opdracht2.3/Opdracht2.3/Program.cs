ede65445e36e6a417fd4294564923eb795a3afac
Rotjochie666 <44999395+RenzoPX@users.noreply.github.com>
2019-03-15 19:39:56 +0100

    Opdracht 2.4

---
+++ Opdracht2.3/Opdracht2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Opdracht2.4/Opdracht2.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Opdracht3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo, Check hier hoe oud je bent!");
+
+            Console.WriteLine("Vul je geboortedatum in (Zoals dit, 20/02/2001)");
+
+            DateTime bday = Convert.ToDateTime(Console.ReadLine());
+            DateTime today = DateTime.Today;
+            int age = today.Year - bday.Year;
+
+            if (bday > today.AddYears(-age))
+                age--;
+
+            Console.WriteLine(age + " Jaar oud");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
