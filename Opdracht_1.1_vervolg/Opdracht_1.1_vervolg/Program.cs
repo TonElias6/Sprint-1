b118224614f66aaa99bb785eb2449e97eb867ec9
Rotjochie666 <44999395+RenzoPX@users.noreply.github.com>
2019-03-15 20:11:35 +0100

    Revert "Opdracht 2.5"
    This reverts commit 86c1dc19a799078d05180bba37df36151a7adc5d.

---
+++ Opdracht_1.1_vervolg/Opdracht_1.1_vervolg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Naam: Mohamed Yasin");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Straat: Wederiklaan 48A");
+            Console.WriteLine("Adress: 9649DA Doplhia");
+            Console.WriteLine("Leeftijd: 16");
+            Console.ReadLine();
+        }
+    }
+}
