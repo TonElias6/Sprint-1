1a4ca0bdd4ad73fc1c4bba67d50e43cd92971286
Mohammed Yasin <0310437@student.rocvantwente.nl>
2019-03-15 19:39:15 +0100

    opdracht 2.5 sprint 1

---
+++ opdracht_2.5/opdracht_2.5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vul je volledige naam in!");
+            string naam = Console.ReadLine();
+            Console.WriteLine("Welkom op de site, " + naam);
+            Console.ReadLine();
+
+            Console.ReadKey();
+
+            Console.Write("Wat is de waarde van de auto!");
+            Console.ReadLine();
+            Console.Write("In euro: ");
+            double waarde = Convert.ToDouble(Console.ReadLine());
+            Console.ReadLine();
+
+            double lakw = waarde * 0.05;
+            double lerenw = waarde * 0.05;
+            double helew = waarde + lakw + lerenw + 1000;
+            Console.WriteLine("Lak kosten: " + lakw + ",- euro");
+            Console.WriteLine("Leren kosten: " + lerenw + ",- euro");
+            Console.WriteLine("Automaat kosten " + "1000,- euro");
+            Console.ReadLine();
+            Console.WriteLine("De gehele kosten zijn: " + helew + ",- euro");
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
